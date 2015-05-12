07c06b62f0e9cf3b8fa4c667b33c2d948c08ab96
Francis Spor <francis@upstatespors.com>
2015-05-11 22:42:47 -0400

    Initial work, I've got the call working, now just making it all cooler, trying to add some humanization

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,7 +15,8 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'humanize'
   ])
   .config(function ($routeProvider) {
     $routeProvider
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,18 @@
  * Controller of the piratesbootyApp
  */
 angular.module('piratesbootyApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, humanize, bootyChecker) {
+    $scope.numberOfCoins = 0;
+
+    $scope.formattedCoins = function() {
+      return humanize.formatNumber($scope.numberOfCoins);
+    }
+
+
+    $scope.checkTheBooty = function () {
+      bootyChecker.findOutHowManyPirates($scope.numberOfPirates)
+        .then(function(data) {
+          $scope.numberOfCoins = data;
+        });
+    }
   });
